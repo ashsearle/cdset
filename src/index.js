afff4ea5701a051dd92627f5766e5c18e5806a2f
Ash Searle <ash@hexmen.com>
2020-01-10 10:55:04 +0000

    DEV: Initial code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
+const cdset = (oldState, path, value) => {
+  // path can be an array or dot-separated string:
+  path.split && (path = path.split("."));
 
-export default deepset;
+  if (path.length === 0) {
+    return value;
+  }
+
+  const key = path[0];
+
+  if (oldState == null) {
+    const looksLikeArrayIndex = String(key) === String(~~key);
+    oldState = looksLikeArrayIndex ? [] : {};
+  }
+
+  // Use Object.assign to handle arrays with extra non-numeric properties:
+  const newState = Object.assign(Array.isArray(oldState) ? [] : {}, oldState);
+  newState[key] = cdset(oldState[key], path.slice(1), value);
+  return newState;
+};
+
+export default cdset;
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,28 @@
-const deepset = require('./index.js');
+const cdset = require("./index.js");
 
-test(() => {
-  expect(deepset({}, 'a', 3)).toEqual({ a: 3 });
+test("sets top-level property", () => {
+  expect(cdset({}, "a", 3)).toEqual({ a: 3 });
+  expect(cdset([], "0", 3)).toEqual([3]);
+  expect(cdset({}, "0", 3)).toEqual({ 0: 3 });
+});
+
+test("sets deeply-nested property", () => {
+  expect(cdset({ p: {} }, "p.a", 3)).toEqual({ p: { a: 3 } });
+  expect(cdset({ p: [] }, "p.0", 3)).toEqual({ p: [3] });
+  expect(cdset({ p: {} }, "p.0", 3)).toEqual({ p: { 0: 3 } });
+});
+
+test("create object/array required to set deeply-nested property", () => {
+  expect(cdset({}, "p.a", 3)).toEqual({ p: { a: 3 } });
+  expect(cdset({}, "p.0", 3)).toEqual({ p: [3] });
+});
+
+test("preserves extra properties on cloned array", () => {
+  const arr = [1, 2, 3];
+  arr.extraProperty = "chips";
+
+  const expected = ["a", 2, 3];
+  expected.extraProperty = "chips";
+
+  expect(cdset(arr, [0], "a")).toEqual(expected);
 });
